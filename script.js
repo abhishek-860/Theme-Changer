376a04073b02e5a4a4d344d819c12eed75bb6179
abhishek-860 <abhishek.singh.951923@gmail.com>
2025-10-06 16:26:09 +0000

    Initial commit

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-
+const button = document.querySelectorAll('.buttons')
+const body = document.querySelector("body")
+button.forEach(function (button) {
+    button.addEventListener('click', function(e){
+        if(e.target.id === 'gray'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'green'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id;
+        }
+    });
+});
